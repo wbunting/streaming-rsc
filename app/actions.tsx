8abfd3010c9d751024d6420d74192b460ec02864
wbunting <bunting.will@gmail.com>
2024-10-17 11:27:12 +0900

    working streaming

---
+++ app/actions.tsx
@@ -0,0 +1,33 @@
+"use server";
+
+import streamReact from "@/lib/streamable-ui/stream-react";
+
+const LoadingComponent = () => (
+  <div className="animate-pulse p-4">getting weather...</div>
+);
+
+const getWeather = async (location: string) => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  return "82°F️ ☀️";
+};
+
+interface WeatherProps {
+  location: string;
+  weather: string;
+}
+
+const WeatherComponent = (props: WeatherProps) => (
+  <div className="border border-neutral-200 p-4 rounded-lg max-w-fit">
+    The weather in {props.location} is {props.weather}
+  </div>
+);
+
+export async function streamComponent() {
+  const result = await streamReact(async function* () {
+    yield <LoadingComponent />;
+    const weather = await getWeather("San Francisco");
+    return <WeatherComponent weather={weather} location="San Francisco" />;
+  });
+
+  return result.value;
+}
---
+++ lib/streamable-ui/stream-react.tsx
@@ -0,0 +1,76 @@
+import RenderResult from "next/dist/server/render-result";
+import { ReactNode } from "react";
+import { createStreamableUI } from "@/lib/streamable-ui/create-streamable-ui";
+
+type Streamable$1 = ReactNode | Promise<ReactNode>;
+type Renderer$1 = () =>
+  | Streamable$1
+  | Generator<Streamable$1, Streamable$1, void>
+  | AsyncGenerator<Streamable$1, Streamable$1, void>;
+
+const streamReact = async (generate: Renderer$1): Promise<RenderResult> => {
+  // Initialize the UI stream with no initial value
+  const ui = createStreamableUI(null);
+
+  async function render({
+    renderer,
+    streamableUI,
+    isLastCall = false,
+  }: {
+    renderer: undefined | Renderer$1;
+    streamableUI: ReturnType<typeof createStreamableUI>;
+    isLastCall?: boolean;
+  }) {
+    if (!renderer) return;
+
+    const rendererResult = renderer();
+
+    if (isAsyncGenerator(rendererResult) || isGenerator(rendererResult)) {
+      while (true) {
+        const { done, value } = await rendererResult.next();
+        const node = await value;
+
+        if (isLastCall && done) {
+          streamableUI.done(node);
+        } else {
+          streamableUI.update(node);
+        }
+
+        if (done) break;
+      }
+    } else {
+      const node = await rendererResult;
+
+      if (isLastCall) {
+        streamableUI.done(node);
+      } else {
+        streamableUI.update(node);
+      }
+    }
+  }
+
+  try {
+    // Render the initial call
+    await render({
+      renderer: generate,
+      streamableUI: ui,
+      isLastCall: true,
+    });
+  } catch (error) {
+    ui.error(error);
+  }
+
+  return {
+    value: ui.value,
+  };
+};
+
+function isAsyncGenerator(value: any): value is AsyncGenerator {
+  return value && typeof value[Symbol.asyncIterator] === "function";
+}
+
+function isGenerator(value: any): value is Generator {
+  return value && typeof value[Symbol.iterator] === "function";
+}
+
+export default streamReact;
